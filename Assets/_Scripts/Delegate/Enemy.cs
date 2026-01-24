1c092f2dd2a49a0fb78ea3812c035021c38f11d1
Kurakin, Alexander <kurakin.alexander.a@gmail.com>
2026-01-24 22:26:12 +1100

    3rd task added

---
+++ Assets/_Scripts/Delegate/Enemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public void Update()
+    {
+        Lifetime += Time.deltaTime;
+    }
+
+    public bool IsDead { get; private set; }
+
+    public float Lifetime { get; private set; }
+
+    public void Kill()
+    {
+        IsDead = true;
+    }    
+}
